1f33e1e03535aea347ac4eb750db63b008b69454
Amy Alsaydi <amyalsaydi@Amys-MacBook-Pro.local>
2019-10-11 17:25:41 -0400

    Completed Q5

--- functionsLabTwo.playground/Contents.swift
+++ functionsLabTwo.playground/Contents.swift
@@ -181,7 +181,18 @@ for (inputOne, inputTwo, expectedOutput) in testCasesFour {
 
 // Write a function that finds the second smallest Int an an array of Ints
 
-// Your function here
+// Answer:
+
+func secondSmallestValue(in numArray: [Int]) -> Int {
+    // find the smallest num and then remove and then assign the smallest num of that array will now be the smallest
+    // OR
+    //use .sorted and then the second smallest is the second index.
+    
+    let sortedIntArray = numArray.sorted()
+    let secondSmallestValue = sortedIntArray[1]
+    
+    return secondSmallestValue
+}
 
 let testCasesFive = [
     (input: [1,2,3,4], expectedOutput: 2),
@@ -191,7 +202,9 @@ let testCasesFive = [
     (input: [807,909,-22,424,244,873,-907,-350,-780,576,-177,278,-855,296,-33,-609,-664,-126,-469,-57,-376,-424,-477,-795,-481,-806,545,727,-879,210,-114,-783,-156,45,781,369,35,900,904,-2,168,489,-329,608,-287,40,442,362,-560,236,-583,698,544,-154,478,535,-397,363,470,-49,49,631,-62,-293,-419,-528,753,-809,-48,-888,606,-889,-908,672,-783,-921,-198,-428,701,-30,106,500,-106,199,-793,392,873,330,366,45,840,649,-135,-850,174,-338,901,-753,402,-62,554,-579,863,955,-999,-13,851,760,-523,-968,225,-173,-605,-759,306,657,844,168,-270,883,963,-835,624,570,705,333,-293,283,382,926,839,264,602,-940,160,-369,-770,-792,722,321,88,-176,-911,203,486,-175,-529,187,-668,518,-282,162,-165,-34,544,538,-828,-562,-154,591,345,-241,962,801,-632,-921,-710,588,-642,113,128,-242,56,716,-709,232,-870,241,-612,-583,-199,762,-601,-410,-896,-493,664,531,-613,-285,-951,-589,243,819,-195,911,-701,-368,-85,-316], expectedOutput: -968)
 ]
 
-//for (input, expectedOutput) in testCasesFive {
-//    let output = secondSmallestValue(in: input)
-//    assert(output == expectedOutput, "Was expecting \(expectedOutput) for input \(input), but got \(output)")
-//}
+for (input, expectedOutput) in testCasesFive {
+    let output = secondSmallestValue(in: input)
+    assert(output == expectedOutput, "Was expecting \(expectedOutput) for input \(input), but got \(output)")
+}
+
+
